d0e52510f3ac3b1f589e9f91733d8f54dd95eb23
Ana Laura Lara Chairez <analauris2103@gmail.com>
2022-11-30 00:05:47 -0600

    0.2 Pantalla Index

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "transpor-t";
+
+    // $host = "213.190.6.148";
+    // $user = "u817166544_codedur";
+    // $pass = "Codedur2040";
+    // $db = "u817166544_codedur";
+
+$conn = mysqli_connect($host,$user,$pass,$db);
---
+++ index.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Transpor-T</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <link href="assets/img/favicon.png" rel="icon">
+
+    <!-- Favicons -->
+
+
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="d-flex align-items-center">
+        <div class="container d-flex align-items-center justify-content-between">
+
+            <h1 class="logo">
+                <a href="index.php"> <img src="assets/img/logo.png" width="120px" alt="logo"></a>
+                </a><span></span></a>
+            </h1>
+
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link active " href="index.php">Inicio</a></li>
+                    <li><a class="nav-link " href="">Rutas</a></li>
+                    <li><a class="nav-link " href="">Monedero</a></li>
+                    <li><a class="nav-link " href="">Nosotros</a></li>
+                    <li><a class="nav-link " href="">Ayuda</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav>
+            <!-- .navbar -->
+
+        </div>
+    </header>
+    <!-- End Header -->
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex align-items-center">
+        <div class="container" data-aos="zoom-out" data-aos-delay="100">
+            <h1 style="color: #FFFFFF;">Bienvenidos a </h1>
+            <h1><span>Transpor-T Dgo</span></h1>
+            <h2 style="color: #FFFFFF;">Una nueva forma de pagar el tranporte público</h2>
+            <div class="d-flex">
+                <a href="" class="btn-get-started scrollto">Conoce más sobre nosotros</a>
+            </div>
+        </div>
+    </section>
+    <!-- End Hero -->
+
+    <br><br>
+    <div class="section-title" style="margin-top:60px" data-aos="fade-up">
+        <h2>Sencilla forma de viajar</h2>
+        <h3>Recarga tu tarjeta<span> y transpórtate</span></h3>
+        <p>En esta aplicación encontrarás un monedero para el control de tu dinero</p>
+        <br><br>
+        <img src="assets/img/credenciales.png" style="width: 50vh;" data-aos="fade-right" data-aos-delay="100">
+    </div>
+
+    <br><br><br>
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about section-bg">
+        <div class="container" data-aos="fade-up">
+
+            <div class="section-title">
+                <h2>RUTAS</h2>
+                <h3>Visualiza el recorrido de <span>las rutas de la ciudad</span></h3>
+                <p>Podrás ver el trayecto de las diferentes rutas y así facilitar tus viajes</p>
+            </div>
+            <center>
+                <div class="row">
+                    <div class="col-lg-4" data-aos="fade-right" data-aos-delay="100">
+                        <img src="https://placehold.jp/200x350.png">
+                    </div>
+                    <div class="col-lg-4" data-aos="fade-right" data-aos-delay="100">
+                        <img src="https://placehold.jp/200x350.png">
+                    </div>
+                    <div class="col-lg-4" data-aos="fade-right" data-aos-delay="100">
+                        <img src="https://placehold.jp/200x350.png">
+                    </div>
+                </div>
+            </center>
+        </div>
+    </section>
+    <!-- End About Section -->
+
+    <br><br>
+    <div class="section-title" style="margin-top:60px" data-aos="fade-up">
+        <h2>Recorridos</h2>
+        <h3>Podrás visualizar tus<span> recorridos anteriores</span></h3>
+        <p>Al visualizar el registro de tus recorridos podrás identificar las rutas que abordaste, la hora y fecha y el costo.</p>
+        <br><br>
+        <img src="https://placehold.jp/200x350.png" data-aos="fade-right" data-aos-delay="100">
+    </div>
+
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact" id="Publicaciones">
+        <div class="container" data-aos="fade-up">
+
+            <div class="section-title">
+
+                <h3><span>Contáctanos</span></h3>
+
+            </div>
+
+            <div class="row" data-aos="fade-up" data-aos-delay="100">
+                <div class="col-lg-6">
+                    <div class="info-box mb-4">
+                        <i class="bx bx-map"></i>
+                        <h3>Nuestra Dirección</h3>
+                        <p>Durango - Mezquital, 34308 Gabino Santillán, Dgo.</p>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="info-box  mb-4">
+                        <i class="bx bx-envelope"></i>
+                        <h3>Correo electrónico</h3>
+                        <p><a href="mailto:info@yoursite.com">transpor-t.dgodgo@gmail.com
+                            </a></p>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="info-box  mb-4">
+                        <i class="bx bx-phone-call"></i>
+                        <h3>Telefono</h3>
+                        <p><a href="tel://6181703043">+52 618 137 3050</a></p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="row" data-aos="fade-up" data-aos-delay="100">
+
+                <div class="col-lg-6 ">
+                    <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14580.640925880973!2d-104.6175808!3d23.9901183!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2392fefbf317535!2sUniversidad%20Tecnol%C3%B3gica%20de%20Durango!5e0!3m2!1ses-419!2smx!4v1669770676348!5m2!1ses-419!2smx" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
+                </div>
+
+                <div class="col-lg-6">
+                    <form method="POST" id="contactForm" name="contactForm" class="contactFormu">
+                        <div class="row">
+
+                            <div class="col-md-6">
+
+                                <div class="form-group mt-3">
+
+                                    <input type="text" class="form-control" name="nombre" id="name" placeholder="Nombre" required>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group mt-3">
+
+                                    <input type="email" class="form-control" name="correo" id="email" placeholder="Correo Electrónico" required>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group mt-3">
+
+                                    <input type="text" class="form-control" name="asunto" id="Asunto" placeholder="Asunto" required>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group mt-3">
+
+                                    <textarea name="mensaje" class="form-control" id="mensaje" cols="30" rows="4" placeholder="Mensaje" style='resize: none;' required></textarea>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group mt-3">
+                                    <input type="submit" class='btn btn-primary' value="Enviar Correo" name="enviar" class="btnenviar">
+                                    <!-- <div class="submitting"><?php include("enviarcorreo.php"); ?> </div> -->
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+    <!-- End Contact Section -->
+
+
+    <main id="main">
+
+    </main>
+    <!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+
+
+
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-3 col-md-6 footer-contact">
+                        <h3><img src="assets/img/logo.png" width="120px" alt="logo"></a>
+                            </a> <span></span></h3>
+                        <p>
+                            Direccion: Durango - Mezquital, <br> 34308 Gabino Santillán, Dgo.<br>
+                            <strong>Telefono:</strong> +52 618 137 3050<br>
+                            <strong>Email:</strong> transpor-t.dgodgo@gmail.com<br>
+                        </p>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Links rapidos</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Inicio</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="">Rutas</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="">Monedero</a></li>
+
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>La empresa</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="">Nosotros</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="">Ayuda</a></li>
+
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Nuestras Redes Sociales</h4>
+
+                        <div class="social-links mt-3">
+                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="container py-4">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Transpor-T Dgo</span></strong>. Todos los derechos reservados.
+            </div>
+            <div class="credits">
+
+                </a>
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer -->
+
+    <!-- API DE WHATS -->
+    <script defer src="https://widget.tochat.be/bundle.js?key=7e2bbdcf-cc9c-44c2-a38d-4545397ac39a"></script>
+    
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center" style="margin-right: 40px;"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
